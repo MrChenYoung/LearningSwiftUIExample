37d3f9799d4a3579db3de2be75e4253bb6f7f22f
code007 <yi_aohehe@163.com>
2022-03-28 10:17:50 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by MrChen on 2022/3/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/LearningSwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIExampleApp.swift
+//  Shared
+//
+//  Created by MrChen on 2022/3/28.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
